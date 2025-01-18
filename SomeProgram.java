49fb6991bbc28f09e8622760f47b783537374451
Paulina Szymanska <pszymanska@student.agh.edu.pl>
2025-01-18 10:31:04 +0100

    2

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,6 @@
 public class SomeProgram { 
     public static void main(String[] args) {
+	int a 6;
       System.out.println("We ereraertetertre learning to use Git.");
      System.out.println("We are learning to use Git."); 
     }
